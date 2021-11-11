0f603999cac2c05858c6fbed952ecb1375463a76
atippaz <tippaz>
2021-11-11 16:07:43 +0700

    prem commit

--- BomberMan/Form1.cs
+++ BomberMan/Form1.cs
@@ -20,6 +20,7 @@ namespace BomberMan
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Hello My Project");
+            MessageBox.Show("i am prem");
         }
     }
 }
