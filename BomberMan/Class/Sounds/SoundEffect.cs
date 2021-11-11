5074c811ab0228486b61ed4439ac97a7f10f1b32
atippaz <tippaz>
2021-11-11 23:19:51 +0700

    Add Background Music and Effect in main form and Add static Class name is "SoundEffect.cs" in folder Class/Sounds

---
+++ BomberMan/Class/Sounds/SoundEffect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.IO;
+using WMPLib;
+
+namespace BomberMan.Class
+{
+    public static class SoundEffect
+    {
+
+        private static SoundPlayer BG;
+        private static WindowsMediaPlayer Effect = new WindowsMediaPlayer();
+
+        public static void BGM_Play()
+        {
+            BG = new SoundPlayer(Properties.Resources.Bgm);
+            BG.PlayLooping();
+        }
+        public static void BGM_Play1()
+        {
+            BG = new SoundPlayer(Properties.Resources.SongLogin);
+            BG.PlayLooping();
+        }
+        public static void BGM_Stop()
+        {
+            BG.Stop();
+        }
+        public static void Effects()
+        { 
+            Effect.URL = Path.GetFullPath("..\\..\\resource\\MusicEffect\\GameEnter.wav");
+        }
+
+    }
+}
