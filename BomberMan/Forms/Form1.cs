fa22407799bbdfa97a65a55e96b1772c5c02c510
atippaz <tippaz>
2021-11-11 22:07:50 +0700

    Move any forms to the new Forms folder

--- BomberMan/Forms/Form1.cs
+++ BomberMan/Forms/Form1.cs
@@ -28,5 +28,10 @@ namespace BomberMan
             GameForm.Show();
             this.Hide();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
