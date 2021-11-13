598c6ad348c594d89a92e621b255a073d35cc536
atippaz <tippaz>
2021-11-13 12:32:25 +0700

    update Class

--- BomberMan/Class/Characters/Characters.cs
+++ BomberMan/Class/Characters/Characters.cs
@@ -5,40 +5,25 @@ namespace BomberMan
     {
         #region Fields
         private int _nHP;
-        private int _nWidth;
-        private int _nHeight;
-        private int _nPositionX;
-        private int _nPositionY;
+        private PictureBox _pbCharacter;
         readonly private int _nAttack;
-        readonly private string _sName;
-        private string _sSourceImage;
         #endregion
         #region Properties
         public int HP { get => _nHP; }
-        public string Name { get => _sName; }
         public int Attack { get => _nAttack; }
-        public int Width { get => _nWidth; set => _nWidth = (value > 0) ? value : 0; }
-        public int Height { get => _nHeight; set => _nHeight = (value > 0) ? value : 0; }
-        public int PositionY { get => _nPositionY; set => _nPositionY = (value > 0) ? value :0; }
-        public int PositionX { get => _nPositionX; set => _nPositionX = (value > 0) ? value : 0; }
-        public string SourceImage { get => _sSourceImage; set => _sSourceImage = value; }
+        public PictureBox Character { get; set; }
         #endregion
         #region Constructors
         public Characters()
         {
-            this._sName = "Player";
+            Character.Name = "Player001";
             this._nHP = 3;
-            this.PositionY = this.PositionX = this.Height = this.Width = 0;
         }
         public Characters(string sName, int nHp, int nPositionX, int nPositionY, int nWidth, int nHeight,string image)
         {
-            this._sName = sName;
+          
             this._nHP = (nHp > 0) ? nHp : 3;
-            this.PositionY = nPositionX;
-            this.PositionX = nPositionY;
-            this.Height = nWidth;
-            this.Width = nHeight;
-            this.SourceImage = image;
+            
         }
         #endregion
         #region Methods
--- BomberMan/Forms/GameForm.cs
+++ BomberMan/Forms/GameForm.cs
@@ -23,22 +23,11 @@ namespace BomberMan
         public Game(string Playername)
         {
             InitializeComponent();
-            playerProperties = new Player($"{Playername}", 3, 0, 0, 0, 0, PlayerImage.Idle);
-            Player = new PictureBox()
-            {
-                Name = $"{Playername}",
-                Size = new Size(60, 50),
-                Location = new Point(100, 100),
-                SizeMode = PictureBoxSizeMode.Zoom,
-                Image = Image.FromFile(Path.GetFullPath(playerProperties.SourceImage)),
-            };
-            this.Controls.Add(Player);
-            Console.WriteLine($"{playerProperties.SourceImage}");
         }
 
         private void TestBtn(object sender, EventArgs e)
         {
-            MessageBox.Show($"{playerProperties.HP} : {playerProperties.Name}");
+           
         }
         void Init(object sender, EventArgs e)
         {
