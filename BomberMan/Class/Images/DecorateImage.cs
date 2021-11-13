a5ea73ed41efa645df89edf6791a5508322f21fe
JENGZY001 <teerapong.saisrikaew250819@gmail.com>
2021-11-13 12:29:03 +0700

    Update and Fix - MainForm - RegisterForm
    Class create
    - Colors
    - DecorateImage
    - PlayerImage

---
+++ BomberMan/Class/Images/DecorateImage.cs
@@ -0,0 +1,7 @@
+﻿namespace BomberMan {
+    static class DecorateImage {
+        static private string PathDecorate = "..\\..\\resource\\DecorateImage\\";
+        static public string LogoGame = PathDecorate + "logo.gif";
+        static public string Blackboard = PathDecorate + "Blackboard.jpg";
+    }
+}
