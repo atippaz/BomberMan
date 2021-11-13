4740c0a8e6a2c8bb6662a8ac232073214b417f4b
atippaz <tippaz>
2021-11-13 23:43:41 +0700

    update LineSide in main form

--- BomberMan/Forms/MainForm.Designer.cs
+++ BomberMan/Forms/MainForm.Designer.cs
@@ -32,15 +32,10 @@
             this.btn_Start = new System.Windows.Forms.Label();
             this.lineSlide1 = new System.Windows.Forms.Label();
             this.timer_Animation = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
             this.btn_EXIT = new System.Windows.Forms.Label();
             this.lineSlide2 = new System.Windows.Forms.Label();
             this.pictureBox_Logo = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Logo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_Start
@@ -49,9 +44,9 @@
             this.btn_Start.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Start.Font = new System.Drawing.Font("Consolas", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Start.ForeColor = System.Drawing.Color.Black;
-            this.btn_Start.Location = new System.Drawing.Point(286, 234);
+            this.btn_Start.Location = new System.Drawing.Point(280, 232);
             this.btn_Start.Name = "btn_Start";
-            this.btn_Start.Size = new System.Drawing.Size(237, 73);
+            this.btn_Start.Size = new System.Drawing.Size(250, 73);
             this.btn_Start.TabIndex = 3;
             this.btn_Start.Text = "START";
             this.btn_Start.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -66,9 +61,9 @@
             this.lineSlide1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.lineSlide1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lineSlide1.Font = new System.Drawing.Font("Consolas", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lineSlide1.Location = new System.Drawing.Point(52, 234);
+            this.lineSlide1.Location = new System.Drawing.Point(280, 308);
             this.lineSlide1.Name = "lineSlide1";
-            this.lineSlide1.Size = new System.Drawing.Size(237, 76);
+            this.lineSlide1.Size = new System.Drawing.Size(0, 2);
             this.lineSlide1.TabIndex = 4;
             this.lineSlide1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -78,27 +73,15 @@
             this.timer_Animation.Interval = 10;
             this.timer_Animation.Tick += new System.EventHandler(this.timer_Animation_Tick);
             // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label1.Font = new System.Drawing.Font("Consolas", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(12, 234);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(277, 177);
-            this.label1.TabIndex = 6;
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // btn_EXIT
             // 
             this.btn_EXIT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.btn_EXIT.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_EXIT.Font = new System.Drawing.Font("Consolas", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_EXIT.ForeColor = System.Drawing.Color.Black;
-            this.btn_EXIT.Location = new System.Drawing.Point(286, 335);
+            this.btn_EXIT.Location = new System.Drawing.Point(280, 332);
             this.btn_EXIT.Name = "btn_EXIT";
-            this.btn_EXIT.Size = new System.Drawing.Size(237, 73);
+            this.btn_EXIT.Size = new System.Drawing.Size(250, 73);
             this.btn_EXIT.TabIndex = 7;
             this.btn_EXIT.Text = "EXIT";
             this.btn_EXIT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -113,9 +96,9 @@
             this.lineSlide2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.lineSlide2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lineSlide2.Font = new System.Drawing.Font("Consolas", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lineSlide2.Location = new System.Drawing.Point(52, 335);
+            this.lineSlide2.Location = new System.Drawing.Point(280, 410);
             this.lineSlide2.Name = "lineSlide2";
-            this.lineSlide2.Size = new System.Drawing.Size(237, 76);
+            this.lineSlide2.Size = new System.Drawing.Size(0, 2);
             this.lineSlide2.TabIndex = 8;
             this.lineSlide2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -128,34 +111,13 @@
             this.pictureBox_Logo.TabIndex = 9;
             this.pictureBox_Logo.TabStop = false;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(236, 251);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(46, 37);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(236, 353);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(46, 37);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 11;
-            this.pictureBox2.TabStop = false;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox_Logo);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_EXIT);
             this.Controls.Add(this.btn_Start);
             this.Controls.Add(this.lineSlide1);
@@ -166,8 +128,6 @@
             this.Text = "THE STUDENT BOMBER";
             this.Load += new System.EventHandler(this.Setup);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Logo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -176,12 +136,9 @@
         private System.Windows.Forms.Label btn_Start;
         private System.Windows.Forms.Label lineSlide1;
         private System.Windows.Forms.Timer timer_Animation;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label btn_EXIT;
         private System.Windows.Forms.Label lineSlide2;
         private System.Windows.Forms.PictureBox pictureBox_Logo;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- BomberMan/Forms/MainForm.cs
+++ BomberMan/Forms/MainForm.cs
@@ -1,43 +1,62 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
 using System.Threading;
 using System.Windows.Forms;
-using System.IO;
 
-namespace BomberMan {
-    public partial class MainForm : Form {
+namespace BomberMan
+{
+    public partial class MainForm : Form
+    {
 
         Thread worker;
-         
-        public MainForm() {
+        PictureBox playerLogo, playerLogo1;
+        public MainForm()
+        {
             InitializeComponent();
-
             // initial value for control
-            this.BackColor = label1.BackColor = Colors.White;
-
             pictureBox_Logo.Image = Image.FromFile(Path.GetFullPath(DecorateImage.LogoGame));
-            pictureBox1.Image = PlayerImage.RunLeft;
-            pictureBox2.Image = PlayerImage.RunUp;
-
-            pictureBox1.Visible= pictureBox2.Visible = false;
+            playerLogo = new PictureBox()
+            {
+                Name = "player",
+                Size = new Size(50, 50),
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Image =  PlayerImage.RunLeft,
+                Location = new Point(btn_Start.Location.X - 60, btn_Start.Location.Y + 20),
+            };
+            playerLogo1 = new PictureBox()
+            {
+                Name = "player",
+                Size = new Size(50, 50),
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Image =  PlayerImage.RunUp,
+                Location = new Point(btn_EXIT.Location.X - 60, btn_EXIT.Location.Y + 20),
+            };
+            this.Controls.Add(playerLogo1);
+            this.Controls.Add(playerLogo);
+            playerLogo.Visible = playerLogo1.Visible = false;
         }
 
-        private void OpenGameForm(object sender, EventArgs e) {
+        private void OpenGameForm(object sender, EventArgs e)
+        {
             RegisterForm registerForm = new RegisterForm();
             registerForm.Show();
             this.Hide();
         }
 
-        private void CloseForm(object sender, EventArgs e) {
+        private void CloseForm(object sender, EventArgs e)
+        {
             Application.Exit();
         }
 
-        private void Setup(object sender, EventArgs e) {
-          worker = new Thread(SoundEffect.Effects);
-          SoundEffect.BGM_Play();
+        private void Setup(object sender, EventArgs e)
+        {
+            worker = new Thread(SoundEffect.Effects);
+            SoundEffect.BGM_Play();
         }
 
-        private void Play_Effect(object sender, EventArgs e) {
+        private void Play_Effect(object sender, EventArgs e)
+        {
             worker = new Thread(SoundEffect.Effects);
             worker.Start();
         }
@@ -46,7 +65,7 @@ namespace BomberMan {
         // PROPERTIES FOR BUTTON
         // ----------------------
 
-        private int lineSpeed = 9;
+        private int lineSpeed = 10;
 
         //--------------
         // START BUTTON 
@@ -55,22 +74,24 @@ namespace BomberMan {
         private bool leave_btnStart = false;
         private bool hover_btnStart = false;
         #endregion
-        private void btn_Start_MouseHover(object sender, EventArgs e) {
+        private void btn_Start_MouseHover(object sender, EventArgs e)
+        {
             btn_Start.BackColor = Colors.OrangeHover;
-
             hover_btnStart = true;
             leave_btnStart = false;
         }
-        private void btn_Start_MouseLeave(object sender, EventArgs e) {
+        private void btn_Start_MouseLeave(object sender, EventArgs e)
+        {
             btn_Start.BackColor = Colors.OrangeLeave;
-
             hover_btnStart = false;
             leave_btnStart = true;
         }
-        private void btn_Start_MouseDown(object sender, MouseEventArgs e) {
+        private void btn_Start_MouseDown(object sender, MouseEventArgs e)
+        {
             btn_Start.BackColor = Colors.OrangeClick;
         }
-        private void btn_Start_MouseClick(object sender, MouseEventArgs e) {
+        private void btn_Start_MouseClick(object sender, MouseEventArgs e)
+        {
             btn_Start.BackColor = Colors.OrangeClick;
             btn_Start.Size = new Size(237, 73);
         }
@@ -82,56 +103,51 @@ namespace BomberMan {
         private bool leave_btnEXIT = false;
         private bool hover_btnEXIT = false;
         #endregion
-        private void btn_EXIT_MouseHover(object sender, EventArgs e) {
+        private void btn_EXIT_MouseHover(object sender, EventArgs e)
+        {
             hover_btnEXIT = true;
             leave_btnEXIT = false;
         }
-        private void btn_EXIT_MouseLeave(object sender, EventArgs e) {
+        private void btn_EXIT_MouseLeave(object sender, EventArgs e)
+        {
             btn_EXIT.BackColor = Colors.GreyLeave;
             hover_btnEXIT = false;
             leave_btnEXIT = true;
         }
-        private void btn_EXIT_MouseClick(object sender, MouseEventArgs e) {
+        private void btn_EXIT_MouseClick(object sender, MouseEventArgs e)
+        {
             btn_EXIT.BackColor = Colors.GreyClick;
         }
-        private void btn_EXIT_MouseDown(object sender, MouseEventArgs e) {
+        private void btn_EXIT_MouseDown(object sender, MouseEventArgs e)
+        {
             btn_EXIT.BackColor = Colors.GreyClick;
         }
 
         // ----------------
         // ANIMATION BUTTON
         // ----------------
-        private void timer_Animation_Tick(object sender, EventArgs e) {
-
-            if (leave_btnStart) {
-                if (lineSlide1.Location.X >= 52) {
-                    lineSlide1.Left -= lineSpeed;
-                }
-                pictureBox1.Visible = false;
+        private void timer_Animation_Tick(object sender, EventArgs e)
+        {
+            if (lineSlide1.Width < btn_Start.Width && hover_btnStart)
+            {
+                lineSlide1.Width += lineSpeed;
+                playerLogo.Visible = true;
             }
-
-            if (hover_btnStart) {
-                if (lineSlide1.Location.X < 286) {
-                    lineSlide1.Left += lineSpeed;
-                }
-                pictureBox1.Visible = true;
+            else if (lineSlide1.Width > 0 && leave_btnStart)
+            {
+                lineSlide1.Width -= lineSpeed;
+                playerLogo.Visible = false;
             }
-
-            if (hover_btnEXIT) {
-                if (lineSlide2.Location.X < 286) {
-                    lineSlide2.Left += lineSpeed;
-                }
-                pictureBox2.Visible = true;
+            if (lineSlide2.Width < btn_Start.Width && hover_btnEXIT)
+            {
+                lineSlide2.Width += lineSpeed;
+                playerLogo1.Visible = true;
             }
-
-            if (leave_btnEXIT) {
-                if (lineSlide2.Location.X >= 52) {
-                    lineSlide2.Left -= lineSpeed;
-                }
-                pictureBox2.Visible = false;
+            else if (lineSlide2.Width > 0 && leave_btnEXIT)
+            {
+                lineSlide2.Width -= lineSpeed;
+                playerLogo1.Visible = false;
             }
         }
-
-
     }
 }
