a33a78bc042213a18d7475c3d3bb6a34af2d6419
atippaz <tippaz>
2021-11-13 22:08:37 +0700

    Add Class Map

---
+++ BomberMan/Class/Maps/Map.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+namespace BomberMan
+{
+    class Map
+    {
+        private PictureBox _pbMap;
+        public PictureBox MapProperties
+        {
+            get => _pbMap;
+        }
+        public Image MapImage
+        {
+            get => _pbMap.Image;
+            set => _pbMap.Image = value;
+        }
+
+        public Map(Image mapImage, Size size)
+        {
+            System.Console.WriteLine($"{mapImage}");
+            this._pbMap = new PictureBox()
+            {
+                Name = "Map",
+                Size = size,
+                Location = new Point(100, 100),
+                Image = mapImage,
+                SizeMode = PictureBoxSizeMode.Zoom,
+            };
+
+        }
+        public void Add(Form form)
+        {
+            form.Controls.Add(_pbMap);
+        }
+        public void AddPicture(PictureBox elements)
+        {
+            this._pbMap.Controls.Add(elements);
+        }
+    }
+}
