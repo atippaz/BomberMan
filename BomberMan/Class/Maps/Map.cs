14e53be8d9abf77316a12ef8d98cdfa779dde075
JENGZY001 <teerapong.saisrikaew250819@gmail.com>
2021-11-16 21:12:45 +0700

    Fix and Update - GameForm

--- BomberMan/Class/Maps/Map.cs
+++ BomberMan/Class/Maps/Map.cs
@@ -17,20 +17,17 @@ namespace BomberMan
 
         public Map(Image mapImage, Size size,Point position,Form form)
         {
-            this._pbMap = new PictureBox()
-            {
+            this._pbMap = new PictureBox() {
                 Name = "Map",
                 Size = size,
                 Location = position,
                 BackgroundImage = mapImage,
+                BorderStyle = BorderStyle.FixedSingle,
                 BackgroundImageLayout = ImageLayout.Tile,
             };
             form.Controls.Add(this._pbMap);
         }
-        public void Add(Form form)
-        {
-            form.Controls.Add(_pbMap);
-        }
+
         public void AddTiles(PictureBox elements)
         {
             this._pbMap.Controls.Add(elements);
