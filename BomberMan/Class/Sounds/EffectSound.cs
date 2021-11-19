2a54161f81d90c5a4ef3c42c3a4cac0d31a232fa
atippaz <tippaz>
2021-11-19 15:34:01 +0700

    update sound

--- BomberMan/Class/Sounds/EffectSound.cs
+++ BomberMan/Class/Sounds/EffectSound.cs
@@ -5,15 +5,9 @@ namespace BomberMan
 {
     public static class EffectSound
     {
-        static Thread _fx;
         private static WindowsMediaPlayer fx = new WindowsMediaPlayer();
 
         public static void Click()
-        {
-            _fx = new Thread(_click);
-            _fx.Start();
-        }
-        static void _click()
         {
             fx.URL = Path.GetFullPath(FxPatch.Path(Effect.Click));
         }
